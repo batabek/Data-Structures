acc1ebd982815ff5681e14cdc1aac10a0965c880
batabek <atabek.berk@gmail.com>
2020-02-09 17:53:18 +0100

    Add the missing interface functions for binary search tree(BST). Update the Readme file.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Data-Structures
-Commonly used data structures written from scratch.
+Some commonly used data structures written from scratch in C++.
 
-* binarysearchtree.h : C++ Template class for the Binary Search Tree (BST) implementation. 
+* binarysearchtree.h : Binary Search Tree (BST) implementation.
+* linklist.h: Singly Linked List for the link list implementation.
+* stack.h: Stack data structure implementation using link list.
+* queue.h: Queue data structure implementation using link list.
+* adgraph.h: Adjacency list based graph implementation.  
--- binarysearchtree.h
+++ binarysearchtree.h
@@ -7,6 +7,8 @@
 // Simple BST with recursively implemented basic operations
 template <typename T>
 class BinarySearchTree {
+
+  // Tree Node Template Class
   template <typename X>
   struct BinarySearchTreeNode {
    public:
@@ -18,9 +20,9 @@ class BinarySearchTree {
     X data;
   };
 
-  // pointer to the root node of the BST
+  // root node of the tree
   BinarySearchTreeNode<T>* root;
-  // #ofnodes found in the tree
+  // number of nodes found in the tree
   unsigned int n;
 
   void insertItem(BinarySearchTreeNode<T>*& node, T value);
@@ -36,7 +38,10 @@ class BinarySearchTree {
     root = nullptr;
   }
 
+void deleteNode(BinarySearchTreeNode<T>* &node, T val);
+
  public:
+
   BinarySearchTree() : root(nullptr), n(0) {}
   ~BinarySearchTree() {
     deleteTree(root);
@@ -44,22 +49,68 @@ class BinarySearchTree {
   }
 
   void insert(T value) { insertItem(root, value); }
+  void deleteItem(T value) { deleteNode(root, value); }
   BinarySearchTreeNode<T>* search(T value) { return searchItem(root, value); }
-  void deleteItem(T value);
-  BinarySearchTreeNode<T>* min();
-  BinarySearchTreeNode<T>* max();
+  BinarySearchTreeNode<T>* findMin();
+  BinarySearchTreeNode<T>* findMax();
+  
+
+  bool isEmpty() const { return size() == 0; }
+  
+  BinarySearchTreeNode<T>* getRoot() const { return root; }
 
   const unsigned int size() const { return n; }
 
+  // Traversals
   void inOrderWalk(BinarySearchTreeNode<T>* node,
                    std::function<void(T val)> nodefunc);
   void preOrderWalk(BinarySearchTreeNode<T>* node,
                     std::function<void(T val)> nodefunc);
   void postOrderWalk(BinarySearchTreeNode<T>* node,
                      std::function<void(T val)> nodefunc);
-
-  BinarySearchTreeNode<T>* getRoot() const { return root; }
 };
+
+/**
+ * @brief      Function to delete a node by searching for its key.
+ *              3 possible options to check
+ *              a) if the node is a leaf node, then simply delete it
+ *              b) if the node has a left/right child, replace its key with the child's
+ *              c) if the node has both left and right child, replace its key with its successor.
+ * @param      node  The node 
+ * @param[in]  val   The key of the node to be deleted
+ */
+template <typename T>
+void BinarySearchTree<T>::deleteNode(BinarySearchTreeNode<T>* &node, T val) {
+
+    // if there is no node nothing to delete
+    if (node == nullptr) {
+      return;
+    } 
+
+    if (val < node->data) {
+       deleteNode(node->left, val); 
+    }
+    else if (val > node->data) {
+        deleteNode(node->right, val);
+    }
+    else if (node->left != nullptr && node->right != nullptr) {
+
+        // find the successor of the right tree
+        BinarySearchTreeNode<T>* cur = node->right;
+        while (cur && cur->left!=nullptr) {
+          cur = cur->left;
+        }
+        // update node key with its successor
+        node->data = cur->data;
+        deleteNode(node->right, cur->data); 
+    }
+    else {
+      BinarySearchTreeNode<T>* temp = node;
+      node = ( node->left != nullptr ) ? node->left : node->right;
+      delete temp; n--;
+    }
+
+}
 
 template <typename T>
 void BinarySearchTree<T>::insertItem(BinarySearchTreeNode<T>*& node, T value) {
@@ -92,9 +143,6 @@ BinarySearchTree<T>::BinarySearchTreeNode<T>* BinarySearchTree<T>::searchItem(
 }
 
 template <typename T>
-void BinarySearchTree<T>::deleteItem(T value) {}
-
-template <typename T>
 void BinarySearchTree<T>::inOrderWalk(BinarySearchTreeNode<T>* node,
                                       std::function<void(T val)> nodefunc) {
   if (node) {
@@ -125,32 +173,30 @@ void BinarySearchTree<T>::postOrderWalk(BinarySearchTreeNode<T>* node,
 }
 
 template <typename T>
-BinarySearchTree<T>::BinarySearchTreeNode<T>* BinarySearchTree<T>::min() {
+BinarySearchTree<T>::BinarySearchTreeNode<T>* BinarySearchTree<T>::findMin() {
   BinarySearchTreeNode<T>* cur = getRoot();
 
   // check for the null case first
   if (cur) {
-    // traverse to the left
+    // then traverse to the left
     while (cur->left != nullptr) {
       cur = cur->left;
     }
   }
-
   return cur;
 }
 
 template <typename T>
-BinarySearchTree<T>::BinarySearchTreeNode<T>* BinarySearchTree<T>::max() {
+BinarySearchTree<T>::BinarySearchTreeNode<T>* BinarySearchTree<T>::findMax() {
   BinarySearchTreeNode<T>* cur = getRoot();
 
   // check for the null case first
   if (cur) {
-    // traverse to the right
+    // then traverse to the right
     while (cur->right != nullptr) {
       cur = cur->right;
     }
   }
-
   return cur;
 }
 
